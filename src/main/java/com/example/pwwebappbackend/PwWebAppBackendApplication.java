8efaf68a746f50a689eea9fffa084f31e9c18b67
diananeg01 <diananegurita07@gmail.com>
2022-10-25 01:10:15 +0300

    initial commit

---
+++ src/main/java/com/example/pwwebappbackend/PwWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pwwebappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwWebAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwWebAppBackendApplication.class, args);
+    }
+
+}
