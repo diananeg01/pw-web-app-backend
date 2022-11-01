2a0966ab0066e29e17f062f16c4ef3ae514950a5
diananeg01 <diananegurita07@gmail.com>
2022-11-01 22:41:12 +0200

    add functionality for book entity

--- src/main/java/com/example/pwwebappbackend/controller/BookController.java
+++ src/main/java/com/example/pwwebappbackend/controller/BookController.java
@@ -1,15 +1,48 @@
 package com.example.pwwebappbackend.controller;
 
+import com.example.pwwebappbackend.model.Book;
 import com.example.pwwebappbackend.service.BookService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@AllArgsConstructor
-@RequestMapping(path = "/book")
+@RequestMapping(path = "/api/books")
 public class BookController {
-    @Autowired
     private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping(path = "/id/{id}")
+    public ResponseEntity<Book> getBookByUuid(@PathVariable() Long id){
+        return new ResponseEntity<>(this.bookService.getBookById(id).get(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/title/{title}")
+    public ResponseEntity<List<Book>> getBooksByTitle(@PathVariable() String title){
+        return new ResponseEntity<>(this.bookService.getBooksByTitle(title).get(), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Book>> getBooks(){
+        return new ResponseEntity<>(this.bookService.getBooks(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<Book> addNewBook(@RequestBody Book newBook) {
+        this.bookService.addNewBook(newBook);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/edit")
+    public ResponseEntity<Book> editBook(@RequestBody Book newBook, @RequestParam String oldTitle, @RequestParam String oldPublishingHouse){
+        this.bookService.editBook(newBook, oldTitle, oldPublishingHouse);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/pwwebappbackend/service/BookServiceImpl.java
+++ src/main/java/com/example/pwwebappbackend/service/BookServiceImpl.java
@@ -1,13 +1,54 @@
 package com.example.pwwebappbackend.service;
 
+import com.example.pwwebappbackend.model.Book;
 import com.example.pwwebappbackend.repo.BookCrudRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service("bookServiceImpl")
 public class BookServiceImpl implements BookService {
     @Autowired
     private final BookCrudRepository bookCrudRepository;
+
+    @Override
+    public Optional<Book> getBookById(Long id) {
+        return this.bookCrudRepository.findById(id);
+    }
+
+    @Override
+    public Optional<List<Book>> getBooksByTitle(String title){
+        return this.bookCrudRepository.findBooksByTitle(title);
+    }
+
+    public List<Book> getBooks() {
+        List<Book> allBooks = new ArrayList<>();
+        this.bookCrudRepository.findAll().forEach(allBooks::add);
+        return allBooks;
+    }
+
+    @Override
+    public void addNewBook(Book newBook) {
+        this.bookCrudRepository.save(newBook);
+    }
+
+    @Override
+    public void editBook(Book newBook, String oldTitle, String oldPublishingHouse) {
+        Optional<Book> book = this.bookCrudRepository.findBookByTitleAndPublishingHouse(oldTitle, oldPublishingHouse);
+        if(book.isPresent()) {
+            Book bookObj = book.get();
+            bookObj.setTitle(newBook.getTitle());
+            bookObj.setAuthor(newBook.getAuthor());
+            bookObj.setPublishingHouse(newBook.getPublishingHouse());
+            bookObj.setDescription(newBook.getDescription());
+            bookObj.setCategory(newBook.getCategory());
+            bookObj.setCover(newBook.getCover());
+            this.bookCrudRepository.save(bookObj);
+        }
+    }
 }
