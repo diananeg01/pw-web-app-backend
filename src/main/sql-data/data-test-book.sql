ead97f9eb66e14f97f4a5cc820a192e058a94fdb
diananeg01 <diananegurita07@gmail.com>
2022-11-02 07:25:16 +0200

    add sql scripts with test data

---
+++ src/main/sql-data/data-test-book.sql
@@ -0,0 +1,50 @@
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '1', 'Imagini ascunse', 'Jason Rekulak', 'AGMADA', 'ausfgc askudfv gaske', 'Horror', 'https://cdn.dc5.ro/img-prod/1931152709-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '2', 'Pasiune simpla', 'Annie Ernaux', 'Anansi', 'ausfgc askudfv gaske', 'Dezvoltare', 'https://cdn.dc5.ro/img-prod/1947733465-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '3', 'Lumina din noi', 'Michelle Obana', 'Litera', 'ausfgc askudfv gaske', 'Dezvoltare', 'https://cdn.dc5.ro/img-prod/1907643517-0-240.png'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '4', 'It starts with us', 'Colleen Hoover', 'Paralela45', 'ausfgc askudfv gaske', 'Dragoste', 'https://cdn.dc5.ro/img-prod/1808352754-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '5', 'It ends with us', 'Colleen Hoover', 'Paralela45', 'ausfgc askudfv gaske', 'Dragoste', 'https://cdn.dc5.ro/img-prod/1808352754-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '6', 'Chainsaw Man, vol.3', 'Tatsuki Fujimoto', '-', 'ausfgc askudfv gaske', 'Manga', 'https://cdn.dc5.ro/img-prod/875397120-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '7', 'Bunny s First Christmas', 'Enid Blyton', 'Cameron', 'ausfgc askudfv gaske', 'Copii', 'https://cdn.dc5.ro/img-prod/939799760-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '8', 'Decoded', 'Phil P. Barden', 'Sutherland', 'ausfgc askudfv gaske', 'Stiinta', 'https://cdn.dc5.ro/img-prod/468056158-0-240.jpeg'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '9', 'Limba germana. Simplu si eficient', 'Orlando Balas', 'Polirom', 'ausfgc askudfv gaske', 'Invatare', 'https://cdn.dc5.ro/img-prod/1920458217-0-240.png'
+         );
+INSERT INTO public.model_book (
+    book_uuid, title, author, publishing_house, description, category, cover
+) VALUES (
+             '10', 'Jurnale de calatorie', 'Albert Camus', 'Polirom', 'ausfgc askudfv gaske', 'Filosofie', 'https://cdn.dc5.ro/img-prod/1962781305-0-240.jpeg'
+         );
